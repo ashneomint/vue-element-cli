21ce65c0c2437818c82ddd97f4bf73dec3fab428
ashneomint <ashneomint@outlook.com>
2022-12-01 15:02:00 +0800

    helloworld

--- springboot/src/main/java/com/ashneomint/springboot/controller/UserController.java
+++ springboot/src/main/java/com/ashneomint/springboot/controller/UserController.java
@@ -1,8 +1,19 @@
 package com.ashneomint.springboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author shiro
  * @date 2022/12/1 14:26
  */
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello~world!";
+    }
 }
